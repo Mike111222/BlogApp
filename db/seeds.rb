b08fa15a9c3da45072fa73874493f1a0c0371ad2
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-04 13:40:56 +0600

    Revert "Update likes counter using seed"
    This reverts commit 907285c12d1556208ba49f38ccca1dba80164931.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,65 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 
 # Create users
-first_user = User.create(name: 'John Doe', email: 'john@example.com', password: 'password')
-second_user = User.create(name: 'Jane Smith', email: 'jane@example.com', password: 'password')
-# Create posts for the first user
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
 4.times do
   first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-  # Create comments for the first post
-  Comment.create(post: first_post, author: second_user, text: 'Nice post!')
-  Comment.create(post: first_post, author: first_user, text: 'Thank you!')
+
+  # Verify the post creation
+  puts "Post Title: #{first_post.title}"
+  puts "Post Author: #{first_post.author.name}"
+
+  # Create comments with the correct author and post
+  comment_author = User.find_by(name: 'Lilly')
+  Comment.create(post: first_post, author: comment_author, text: 'Hi Tom!', post_id: first_post.id)
+  Comment.create(post: first_post, author: comment_author, text: 'Nice post!', post_id: first_post.id)
+  Comment.create(post: first_post, author: comment_author, text: 'Keep it up!', post_id: first_post.id)
+  Comment.create(post: first_post, author: comment_author, text: 'Great work!', post_id: first_post.id)
+  Comment.create(post: first_post, author: comment_author, text: 'Awesome!', post_id: first_post.id)
+  Comment.create(post: first_post, author: comment_author, text: 'Well done!', post_id: first_post.id)
+
+  # Verify the comments
+  puts "Comments for Post: #{first_post.comments.count}"
 end
-# Create posts for the second user
-4.times do
-  second_post = Post.create(author: second_user, title: 'Greetings', text: 'This is my second post')
-end
+
 # Find, update, and delete entities
 user = User.find_by(name: 'Tom')
 puts "User: #{user.name}"
 user.update(name: 'Tom Smith')
 updated_user = User.find_by(name: 'Tom Smith')
 puts "Updated User: #{updated_user.name}"
+
 post = updated_user.posts.find_by(title: 'Hello')
 puts "Post Title: #{post.title}" if post
 post.update(title: 'Greetings', text: 'This is my updated post') if post
 updated_post = updated_user.posts.find_by(title: 'Greetings')
 puts "Updated Post Title: #{updated_post.title}" if updated_post
+
 comment = updated_post.comments.find_by(text: 'Hi Tom!') if updated_post
 puts "Comment Text: #{comment.text}" if comment
 comment.destroy if comment
 puts "Comment Count after deletion: #{updated_post.comments.count}" if updated_post
+
 # Call the methods
 recent_posts = user.recent_posts
 puts "Recent Posts for User: #{user.name}"
 recent_posts.each do |post|
   puts "Title: #{post.title}, Text: #{post.text}"
 end
+
 updated_user.update_posts_counter
 puts "Posts Counter for User: #{updated_user.name} - #{updated_user.posts_counter}"
+
 recent_comments = updated_post.recent_comments
 puts "Recent Comments for Post: #{updated_post.title}"
 recent_comments.each do |comment|
   puts "Text: #{comment.text}"
 end
+
 updated_post.update_comments_counter
-puts "Comments Counter for Post: #{updated_post.title} - #{updated_post.comments_counter}"
-updated_post.update_likes_counter
-puts "Likes Counter for Post: #{updated_post.title} - #{updated_post.likes_counter}"
 puts "Comments Counter for Post: #{updated_post.title} - #{updated_post.comments_counter}"
 
 updated_post.update_likes_counter
